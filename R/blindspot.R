dfc0abfbe719471c1ed16443f7d297e90688f74c
Mears-UFL <mears@ufl.edu>
2016-02-02 18:20:25 -0500

    Update blindspot.R

--- R/blindspot.R
+++ R/blindspot.R
@@ -1,11 +1,24 @@
+library("dplyr")
+library("ggplot2")
+library("tidyr")
 
+# Example to hardcode data file: 
+# getwd() # find project folder & paste location as string for path
+# f_path <- "/Users/R/Documents/Teaching/Spring2016/SensLab/CogLabs/"  # paste address string in quotes
+# f_csv<-"SignalDetection/SigDet.csv"  # filename string
+# data_set<-read.csv(file= paste0(f_path,f_csv), header = TRUE, stringsAsFactors = FALSE)
 
-blind_spot_tidy <- gather(blind_spot,View,X.Coordinate,Disappeared,Reappeared)
+data_set <- read.csv(file = "class_blind_spot.csv", header = TRUE, stringsAsFactors = FALSE)
+blind_spot <- as.tbl(data_set)
+
+# Tidy up data set for plotting
+blind_spot_tidy <- gather(blind_spot, View, X.Coordinate, Disappeared, Reappeared)
 
 # Tile plot of raw data
 ggplot(blind_spot_tidy, aes(x = X.Coordinate, y = Y.Coordinate, fill = View)) + 
   geom_tile() + facet_wrap(~randGator)
 
+# Filter out y-axis observations w/o blindspot (to see data more clearly)
 blind_spot_nreap <- blind_spot %>% 
   filter(Reappeared >= Disappeared)
 
