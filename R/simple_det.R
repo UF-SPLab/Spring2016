303da9fa9a66e71c1a10afec675bde6d981e4803
Mears-UFL <mears@ufl.edu>
2016-02-02 17:53:19 -0500

    Update simple_det.R

--- R/simple_det.R
+++ R/simple_det.R
@@ -13,17 +13,21 @@ library("tidyr")
 data_set <- read.csv(file = "class_simple_det.csv", header = TRUE, stringsAsFactors = FALSE)
 vis_search <- as.tbl(data_set)
 
-vis_search_subj <- vis_search %>% unite(Distractor_Target, Distractor.type,Target) %>% 
-  group_by(Distractor_Target,Number.of.distractors,randGator) %>% tally(mean(RT..ms.))
+vis_search_subj <- vis_search %>% 
+  unite(Distractor_Target, Distractor.type, Target) %>% 
+  group_by(Distractor_Target, Number.of.distractors, randGator) %>% 
+  tally(mean(RT..ms.))
 
-ggplot(vis_search_subj,aes(x=Number.of.distractors,y=n))+
-  geom_line(size=1.5,aes(color=factor(Distractor_Target)))+
-  geom_point(size=4,aes(shape=factor(Distractor_Target),color=Distractor_Target))+
-  facet_wrap(~randGator)+ theme_minimal()
+ggplot(vis_search_subj, aes(x = Number.of.distractors, y = n)) +
+  geom_line(size = 1.5, aes(color = factor(Distractor_Target))) +
+  geom_point(size = 4, aes(shape = factor(Distractor_Target), color = Distractor_Target)) +
+  facet_wrap(~randGator) + theme_minimal()
 
-vis_search_grp<-vis_search_subj %>% group_by(Distractor_Target,Number.of.distractors) %>% tally(mean(n))
+vis_search_grp <- vis_search_subj %>% 
+  group_by(Distractor_Target, Number.of.distractors) %>% 
+  tally(mean(n))
 
-ggplot(vis_search_grp,aes(x=Number.of.distractors,y=n))+
-  geom_line(size=1.5,aes(color=Distractor_Target))+
-  geom_point(size=4,aes(shape=factor(Distractor_Target),color=Distractor_Target))+
-  scale_y_continuous("RT (ms)")+ theme_minimal()
+ggplot(vis_search_grp, aes( x = Number.of.distractors, y = n)) +
+  geom_line(size = 1.5, aes(color = Distractor_Target)) +
+  geom_point(size = 4, aes(shape = factor(Distractor_Target), color = Distractor_Target)) +
+  scale_y_continuous("RT (ms)") + theme_minimal()
